87a1ad37d893736a20d4c3ebe1082c5bef0eccee
Eunwoo Choi <61371424+eunwoo1104@users.noreply.github.com>
2021-07-23 08:58:49 +0900

    docs: added docs to submit_manager

--- module/submit_manager.py
+++ module/submit_manager.py
@@ -9,13 +9,24 @@ class SubmitManager:
         self.approved_submits = approved_submits
 
     def get_submits(self) -> Dict[int, Bot]:
+        """
+        봇 신청 리스트를 ID: Bot 형식의 dict로 반환합니다.
+        :return: Dict[int, submits.Bot]
+        """
         return {x.id: x for x in self.submits}
 
     def get_submit(self, query: int, safe: bool = True) -> Optional[Bot]:
+        """
+        봇 신청 객체를 query 파라메터에 따라 가져옵니다. 봇이 존재하지 않는 경우 ``None``을 반환합니다.
+
+        :param query: 봇 ID 또는 신청 순서.
+        :param safe: 봇이 존재하지 않는 경우, 이 값이 ``True``일 경우에는 ``None``을 반환하고, ``False``인 경우에는 예외를 발생시킵니다.
+        :return: Optional[submits.Bot]
+        :raises: KeyError - 봇이 존재하지 않습니다.
+        """
         subs = self.get_submits()
         if query not in subs:
-
-            if query < 1000000000:  # Maximum ID num is 1000000000
+            if query < 1000000000:  # Maximum ID value is 1000000000
                 # Maybe it is index?
                 with suppress(IndexError):
                     return self.submits[query-1]
@@ -26,6 +37,11 @@ class SubmitManager:
         return subs[query]
 
     def __del_from_list(self, query: int) -> Optional[Bot]:
+        """
+        SubmitManager 내부 전용 함수입니다. 봇 신청 리스트에서 해당 봇을 삭제합니다. 봇이 존재하지 않다면 ``None``을 반환하고,
+        삭제에 성공했다면 해당 봇을 반환합니다.
+        :return: Optional[submits.Bot]
+        """
         if isinstance(query, Bot):
             query = query.id
         subs = self.get_submits()
@@ -36,6 +52,14 @@ class SubmitManager:
         return bot
 
     def approve(self, query: Union[int, Bot], safe: bool = True) -> Optional[Bot]:
+        """
+        봇 신청을 승인합니다. 성공적으로 승인했다면 승인한 봇 객체를 반환합니다.
+
+        :param query: 봇 ID 또는 봇 객체.
+        :param safe: 봇이 존재하지 않는 경우, 이 값이 ``True``일 경우에는 ``None``을 반환하고, ``False``인 경우에는 예외를 발생시킵니다.
+        :return: Optional[submits.Bot]
+        :raises: KeyError - 봇이 존재하지 않습니다.
+        """
         if isinstance(query, Bot):
             query = query.id
         res = self.__del_from_list(query)
@@ -48,6 +72,14 @@ class SubmitManager:
         return res
 
     def deny(self, query: Union[int, Bot], safe: bool = True) -> Optional[Bot]:
+        """
+        봇 신청을 거부합니다. 성공적으로 거부했다면 거부한 봇 객체를 반환합니다.
+
+        :param query: 봇 ID 또는 봇 객체.
+        :param safe: 봇이 존재하지 않는 경우, 이 값이 ``True``일 경우에는 ``None``을 반환하고, ``False``인 경우에는 예외를 발생시킵니다.
+        :return: Optional[submits.Bot]
+        :raises: KeyError - 봇이 존재하지 않습니다.
+        """
         if isinstance(query, Bot):
             query = query.id
         res = self.__del_from_list(query)
